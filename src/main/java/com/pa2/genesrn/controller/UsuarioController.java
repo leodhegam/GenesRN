540d51cfbd744003507a12afb80df6cc4398cca6
leodhegam <leonardo.macedo.058@ufrn.edu.br>
2021-11-13 20:43:34 -0300

    adicionada validação de email

--- src/main/java/com/pa2/genesrn/controller/UsuarioController.java
+++ src/main/java/com/pa2/genesrn/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -20,6 +21,8 @@ import javax.validation.Valid;
 
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioService usuarioService;
     @Autowired
     private UsuarioRepository usuarioRepository;
 
@@ -43,9 +46,19 @@ public class UsuarioController {
     }
 
     @PostMapping("/cadastrar")
-    public String cadastrar(@ModelAttribute @Valid Usuario usuario, HttpSession session, Errors errors) {
+    public String cadastrar(@ModelAttribute @Valid Usuario usuario, HttpSession session, Errors errors, BindingResult bindingResult) {
+//        Usuario userExists = usuarioService.findUserByEmail(usuario.getEmail());
+//        if(userExists != null){
+//            session.setAttribute("message", "E-mail já existente");
+//        }
+        Usuario usuarioExists = usuarioRepository.findByEmail(usuario.getEmail());
+        if(usuarioExists != null){
+            session.setAttribute("messageError", "E-mail já existente");
+            return "redirect:/";
+        }
         if (errors.hasErrors()) {
-            return "cadastrar";
+            session.setAttribute("message", "Verifique os campos");
+            return "/";
         } else {
             usuario.setSenha(bp.encode(usuario.getSenha()));
             usuarioRepository.save(usuario);
--- src/main/java/com/pa2/genesrn/service/UsuarioService.java
+++ src/main/java/com/pa2/genesrn/service/UsuarioService.java
@@ -2,12 +2,17 @@ package com.pa2.genesrn.service;
 
 import com.pa2.genesrn.model.Usuario;
 import com.pa2.genesrn.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioService {
 
     UsuarioRepository usuarioRepository;
 
     public Usuario save(Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+    public Usuario findUserByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
     }
 }
