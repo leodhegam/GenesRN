71ab4d9eae6c4c96394995cc54b5d0ab5d49da46
marcoscarvalho99 <marcoscarvalho99@yahoo.com.br>
2021-12-03 13:53:02 -0300

    adição do front telas de cadastro e de login(com um defeito erro no nao existe usuario)!

--- src/main/java/com/pa2/genesrn/controller/UsuarioController.java
+++ src/main/java/com/pa2/genesrn/controller/UsuarioController.java
@@ -13,8 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
