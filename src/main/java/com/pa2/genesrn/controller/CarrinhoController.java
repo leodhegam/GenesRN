2330cff1c9cdea98d2d1c619302c1917d1bd64ef
alysonmiguel <alysonmiguel16@gmail.com>
2022-01-07 16:14:43 -0300

    Iniciando o carrinho

---
+++ src/main/java/com/pa2/genesrn/controller/CarrinhoController.java
@@ -0,0 +1,88 @@
+package com.pa2.genesrn.controller;
+
+
+import com.pa2.genesrn.util.CookieUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController(value = "/loja")
+public class CarrinhoController {
+
+    @GetMapping("/add-item/{id}")
+    public ResponseEntity<?> adicionarCarrinho(@PathVariable(name = "id") Long id, HttpServletResponse response, HttpServletRequest request) {
+        Cookie[] cookiesRequest = request.getCookies();
+        System.out.println("Passou aqui ");
+        try{
+            if (CookieUtil.verificaCookies(cookiesRequest, "produtos")){
+                Cookie cookie = CookieUtil.procurarCookie(cookiesRequest, "produtos");
+
+                String valorCookie = cookie.getValue();
+
+                if(!valorCookie.contains(id.toString())){
+                    valorCookie += id + "/";
+
+                    cookie.setValue(valorCookie);
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+
+            }else{
+                Cookie c = new Cookie("produtos", id+"/");
+                c.setMaxAge(60 * 60 * 24 * 30); // 30 dias
+                c.setPath("/");
+                response.addCookie(c);
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+
+        return ResponseEntity.ok("produto adicionado");
+    }
+
+    @GetMapping("/remove-item/{id}")
+    public ResponseEntity<?> removerItemCookie(@PathVariable(name = "id") Long id, HttpServletResponse response, HttpServletRequest request){
+        Cookie[] cookiesRequest = request.getCookies();
+
+        try{
+            if (CookieUtil.verificaCookies(cookiesRequest, "produtos")){
+                Cookie cookie = CookieUtil.procurarCookie(cookiesRequest, "produtos");
+
+                String valorCookie = cookie.getValue();
+
+                if(valorCookie.contains(id.toString())){
+                    String[] ids = valorCookie.split("/");
+                    StringBuilder novoCookie = new StringBuilder();
+
+                    for (String s : ids) {
+                        if(!s.equals(id.toString())){
+                            novoCookie.append(s).append("/");
+                        }
+                    }
+
+                    cookie.setValue(novoCookie.toString());
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+
+            }else{
+                return ResponseEntity.badRequest().build();
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+
+        return ResponseEntity.ok("produto removido");
+    }
+}
+
+
